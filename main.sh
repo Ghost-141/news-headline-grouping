d6c888e88c774de41ff9765210e446b8e9beb0e1
Imtiaz Ahammed <117445651+Ghost-141@users.noreply.github.com>
2025-12-17 10:27:10 +0600

    updated .sh script
    updated main.sh script for navigating to project folder to avoid any relative path error

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd home/imtiaz/Downloads/Scrapping\ Codes || exit 1
 source /home/imtiaz/Downloads/Scrapping\ Codes/.venv/bin/activate
 python /home/imtiaz/Downloads/Scrapping\ Codes/main.py
